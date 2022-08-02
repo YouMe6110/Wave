ad9ac762348b7cb588c445b5bcf3829aec830969
YouMe6110 <ume0116@naver.com>
2022-08-02 17:16:18 +0900

    DAY 05

--- wave.js
+++ wave.js
@@ -3,8 +3,11 @@ import {
 } from './point.js';
 
 export class Wave {
-    constructor() {
-
+    constructor(index, totalPoints, color) {
+        this.index = index;
+        this.totalPoints = totalPoints;
+        this.color = color;
+        this.point = [];
     }
 
     resize(stageWidth, stageHeight) {
@@ -13,21 +16,34 @@ export class Wave {
 
         this.centerX = stageWidth / 2;
         this.centerY = stageHeight / 2;
+        
+        this.pointGap = this.stageWidth / (this.totalPoints - 1);
 
         this.init();
     }
 
     init() {
-        this.point = new Point(this.centerX, this.centerY);
+        this.point = [];
+
+        for (let i = 0; i < this.totalPoints; i++) {
+            const point = new Point(this.index + i, this.pointGap * i, this.centerY);
+            this.point[i] = point;
+        }
     }
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.fillStyle = '#ff0000';
+        ctx.fillStyle = this.color;
 
-        this.point.update();
+        let prevX = this.points[0].x;
+        let prevY = this.points[0].y;
 
-        ctx.arc(this.point.x, this.point.y, 30, 0, 2 * Math.PI);
-        ctx.fill();
+        ctx.moveTo(prevX, prevY);
+
+        for (let i = 0; i < this.totalPoints; i++) {
+            if (i < this.totalPoints - 1) {
+                this.points[i].update();
+            }
+        }
     }
 }
